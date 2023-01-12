b5fcedf9d7f3703386073bef1ab4feeae650742d
Mathias Schaefer (molily) <molily@mailbox.org>
2023-01-12 10:05:17 +0100

    Add example for Vitest that would make the test pass

--- dom.test.js
+++ dom.test.js
@@ -1,23 +1,26 @@
 import { fireEvent } from '@testing-library/dom';
 
-beforeAll(() => {
+/**
+ * JSDOM does not have the window.PointerEvent constructor,
+ * so we mock it for the test.
+ */
+class PointerEventFake extends Event {
   /**
-   * JSDOM does not have the window.PointerEvent constructor,
-   * so we mock it for the test.
+   * @param {string} type
+   * @param {PointerEventInit} [props]
    */
-  class PointerEventFake extends Event {
-    /**
-     * @param {string} type
-     * @param {PointerEventInit} [props]
-     */
-    constructor(type, props) {
-      super(type, props);
-      if (props && props.pointerType) {
-        this.pointerType = props.pointerType;
-      }
+  constructor(type, props) {
+    super(type, props);
+    if (props && props.pointerType) {
+      this.pointerType = props.pointerType;
     }
   }
+}
+
+beforeAll(() => {
   window.PointerEvent = PointerEventFake;
+  // For vitest, this is necessary since window is not document.defaultView:
+  // document.defaultView.PointerEvent = PointerEvent;
 });
 
 it("works", () => {
